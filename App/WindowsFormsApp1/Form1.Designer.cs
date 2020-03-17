fea461b69293286555953e866008a308c6d945e5
Broek,Ryan R. van den <i432004@fhict.nl>
2020-03-17 11:40:27 +0100

    changed label names to avoid future merge conflicts

--- App/WindowsFormsApp1/Form1.Designer.cs
+++ App/WindowsFormsApp1/Form1.Designer.cs
@@ -31,28 +31,28 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lblPSearch = new System.Windows.Forms.Label();
+            this.tbSearch = new System.Windows.Forms.TextBox();
             this.lblDepartment = new System.Windows.Forms.Label();
             this.lblStock = new System.Windows.Forms.Label();
             this.lblPrice = new System.Windows.Forms.Label();
             this.lblName = new System.Windows.Forms.Label();
             this.lblType = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.lablePInormation = new System.Windows.Forms.Label();
+            this.labelPProducts = new System.Windows.Forms.Label();
             this.btUpdate = new System.Windows.Forms.Button();
             this.btRequest = new System.Windows.Forms.Button();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.labelPDepartment = new System.Windows.Forms.Label();
+            this.labelPCurrentlyInStock = new System.Windows.Forms.Label();
+            this.labelPPrice = new System.Windows.Forms.Label();
+            this.labelPName = new System.Windows.Forms.Label();
+            this.labelPType = new System.Windows.Forms.Label();
             this.btAdd = new System.Windows.Forms.Button();
             this.btRemove = new System.Windows.Forms.Button();
             this.btCrease = new System.Windows.Forms.Button();
             this.lbProducts = new System.Windows.Forms.ListBox();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.tbSearch = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.SuspendLayout();
@@ -83,22 +83,22 @@
             // tabPage2
             // 
             this.tabPage2.BackColor = System.Drawing.Color.Salmon;
-            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.lblPSearch);
             this.tabPage2.Controls.Add(this.tbSearch);
             this.tabPage2.Controls.Add(this.lblDepartment);
             this.tabPage2.Controls.Add(this.lblStock);
             this.tabPage2.Controls.Add(this.lblPrice);
             this.tabPage2.Controls.Add(this.lblName);
             this.tabPage2.Controls.Add(this.lblType);
-            this.tabPage2.Controls.Add(this.label8);
-            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.lablePInormation);
+            this.tabPage2.Controls.Add(this.labelPProducts);
             this.tabPage2.Controls.Add(this.btUpdate);
             this.tabPage2.Controls.Add(this.btRequest);
-            this.tabPage2.Controls.Add(this.label7);
-            this.tabPage2.Controls.Add(this.label4);
-            this.tabPage2.Controls.Add(this.label3);
-            this.tabPage2.Controls.Add(this.label2);
-            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.labelPDepartment);
+            this.tabPage2.Controls.Add(this.labelPCurrentlyInStock);
+            this.tabPage2.Controls.Add(this.labelPPrice);
+            this.tabPage2.Controls.Add(this.labelPName);
+            this.tabPage2.Controls.Add(this.labelPType);
             this.tabPage2.Controls.Add(this.btAdd);
             this.tabPage2.Controls.Add(this.btRemove);
             this.tabPage2.Controls.Add(this.btCrease);
@@ -109,6 +109,23 @@
             this.tabPage2.Size = new System.Drawing.Size(790, 470);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Products";
+            // 
+            // lblPSearch
+            // 
+            this.lblPSearch.AutoSize = true;
+            this.lblPSearch.Location = new System.Drawing.Point(6, 437);
+            this.lblPSearch.Name = "lblPSearch";
+            this.lblPSearch.Size = new System.Drawing.Size(67, 20);
+            this.lblPSearch.TabIndex = 31;
+            this.lblPSearch.Text = "Search:";
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Location = new System.Drawing.Point(79, 434);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(185, 27);
+            this.tbSearch.TabIndex = 30;
+            this.tbSearch.TextChanged += new System.EventHandler(this.tbSearch_TextChanged);
             // 
             // lblDepartment
             // 
@@ -155,24 +172,24 @@
             this.lblType.TabIndex = 25;
             this.lblType.Text = "***";
             // 
-            // label8
+            // lablePInormation
             // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(410, 45);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(98, 18);
-            this.label8.TabIndex = 24;
-            this.label8.Text = "Information:";
+            this.lablePInormation.AutoSize = true;
+            this.lablePInormation.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lablePInormation.Location = new System.Drawing.Point(410, 45);
+            this.lablePInormation.Name = "lablePInormation";
+            this.lablePInormation.Size = new System.Drawing.Size(98, 18);
+            this.lablePInormation.TabIndex = 24;
+            this.lablePInormation.Text = "Information:";
             // 
-            // label5
+            // labelPProducts
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(3, 3);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(81, 20);
-            this.label5.TabIndex = 23;
-            this.label5.Text = "Products:";
+            this.labelPProducts.AutoSize = true;
+            this.labelPProducts.Location = new System.Drawing.Point(3, 3);
+            this.labelPProducts.Name = "labelPProducts";
+            this.labelPProducts.Size = new System.Drawing.Size(81, 20);
+            this.labelPProducts.TabIndex = 23;
+            this.labelPProducts.Text = "Products:";
             // 
             // btUpdate
             // 
@@ -194,55 +211,55 @@
             this.btRequest.Text = "Request restock";
             this.btRequest.UseVisualStyleBackColor = true;
             // 
-            // label7
+            // labelPDepartment
             // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(352, 198);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(97, 17);
-            this.label7.TabIndex = 12;
-            this.label7.Text = "Department:";
+            this.labelPDepartment.AutoSize = true;
+            this.labelPDepartment.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPDepartment.Location = new System.Drawing.Point(352, 198);
+            this.labelPDepartment.Name = "labelPDepartment";
+            this.labelPDepartment.Size = new System.Drawing.Size(97, 17);
+            this.labelPDepartment.TabIndex = 12;
+            this.labelPDepartment.Text = "Department:";
             // 
-            // label4
+            // labelPCurrentlyInStock
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(309, 171);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(140, 17);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Currently in stock;";
+            this.labelPCurrentlyInStock.AutoSize = true;
+            this.labelPCurrentlyInStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPCurrentlyInStock.Location = new System.Drawing.Point(309, 171);
+            this.labelPCurrentlyInStock.Name = "labelPCurrentlyInStock";
+            this.labelPCurrentlyInStock.Size = new System.Drawing.Size(140, 17);
+            this.labelPCurrentlyInStock.TabIndex = 9;
+            this.labelPCurrentlyInStock.Text = "Currently in stock;";
             // 
-            // label3
+            // labelPPrice
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(399, 145);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(50, 17);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Price:";
+            this.labelPPrice.AutoSize = true;
+            this.labelPPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPPrice.Location = new System.Drawing.Point(399, 145);
+            this.labelPPrice.Name = "labelPPrice";
+            this.labelPPrice.Size = new System.Drawing.Size(50, 17);
+            this.labelPPrice.TabIndex = 8;
+            this.labelPPrice.Text = "Price:";
             // 
-            // label2
+            // labelPName
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(395, 114);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 17);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Name:";
+            this.labelPName.AutoSize = true;
+            this.labelPName.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPName.Location = new System.Drawing.Point(395, 114);
+            this.labelPName.Name = "labelPName";
+            this.labelPName.Size = new System.Drawing.Size(54, 17);
+            this.labelPName.TabIndex = 7;
+            this.labelPName.Text = "Name:";
             // 
-            // label1
+            // labelPType
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(400, 86);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 17);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Type:";
+            this.labelPType.AutoSize = true;
+            this.labelPType.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPType.Location = new System.Drawing.Point(400, 86);
+            this.labelPType.Name = "labelPType";
+            this.labelPType.Size = new System.Drawing.Size(49, 17);
+            this.labelPType.TabIndex = 6;
+            this.labelPType.Text = "Type:";
             // 
             // btAdd
             // 
@@ -303,23 +320,6 @@
             this.tabPage4.Text = "Statistics";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
-            // tbSearch
-            // 
-            this.tbSearch.Location = new System.Drawing.Point(79, 434);
-            this.tbSearch.Name = "tbSearch";
-            this.tbSearch.Size = new System.Drawing.Size(185, 27);
-            this.tbSearch.TabIndex = 30;
-            this.tbSearch.TextChanged += new System.EventHandler(this.tbSearch_TextChanged);
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 437);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(67, 20);
-            this.label6.TabIndex = 31;
-            this.label6.Text = "Search:";
-            // 
             // MediaBazaar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -341,11 +341,11 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Button btRequest;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelPDepartment;
+        private System.Windows.Forms.Label labelPCurrentlyInStock;
+        private System.Windows.Forms.Label labelPPrice;
+        private System.Windows.Forms.Label labelPName;
+        private System.Windows.Forms.Label labelPType;
         private System.Windows.Forms.Button btAdd;
         private System.Windows.Forms.Button btRemove;
         private System.Windows.Forms.Button btCrease;
@@ -353,14 +353,14 @@
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TabPage tabPage4;
         private System.Windows.Forms.Button btUpdate;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lablePInormation;
+        private System.Windows.Forms.Label labelPProducts;
         private System.Windows.Forms.Label lblDepartment;
         private System.Windows.Forms.Label lblStock;
         private System.Windows.Forms.Label lblPrice;
         private System.Windows.Forms.Label lblName;
         private System.Windows.Forms.Label lblType;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblPSearch;
         private System.Windows.Forms.TextBox tbSearch;
     }
 }
